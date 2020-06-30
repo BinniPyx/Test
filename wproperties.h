f66aaeca77128d2bc69993fd2c2e46bbcb80b411
Alex <aleks.kalinichenko.98@mail.ru>
2020-06-30 19:20:20 +0300

    add conf.txt and Параметры

--- wproperties.h
+++ wproperties.h
@@ -1,7 +1,8 @@
 #ifndef WPROPERTIES_H
 #define WPROPERTIES_H
-
+#include <QListWidget>
 #include <QWidget>
+#include <QListWidgetItem>
 
 namespace Ui {
 class WProperties;
@@ -13,10 +14,21 @@ class WProperties : public QWidget
 
 public:
     explicit WProperties(QWidget *parent = nullptr);
+    QListWidgetItem currentItem(QListWidgetItem *parent=nullptr);
     ~WProperties();
-
+signals:
+    void signalSetPage1();
+    void signalSetPage2();
 private:
     Ui::WProperties *ui;
+
+protected:
+
+    void mousePressEvent(QMouseEvent *event) override;
+private slots:
+    void slotSetPage1();
+    void slotSetPage2();
+    void on_comboBox_3_currentTextChanged(const QString &arg1);
 };
 
 #endif // WPROPERTIES_H
